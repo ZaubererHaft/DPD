b2b5ff925a0f198f2a4c504077bab363107a6aab
= <=>
2020-06-04 08:59:04 +0200

    added adapter pattern

--- definitions/misc.sql
+++ definitions/misc.sql
@@ -6,16 +6,20 @@ select m.name, m.isstatic, c.name from method m join classifier c on m.classifie
 select * from methodinvocation mi join classifier c on c.id = mi.classifier_id join method m on mi.method_id = m.id join classifier c2 on m.classifier_id = c2.id;
 
 
-ToDo: methode muss aus interface kommen
+-- proxy
 SELECT * FROM classifier i WHERE i.type = 'INTERFACE' AND EXISTS (SELECT * FROM derivation d1 WHERE d1.target_id = i.id 
                                                       AND EXISTS (SELECT * FROM method m WHERE m.classifier_id = d1.source_id
                                                       AND EXISTS (SELECT * FROM method m2 WHERE m2.name = m.name AND m2.classifier_id = i.id
                                                       AND EXISTS (SELECT * FROM methodinvocation mi WHERE mi.classifier_id = i.id AND mi.method_id = m.id))));
 
--- problem: vom selben typ?
+-- strategy
 SELECT * FROM classifier c1 WHERE c1.type IN ('ABSTRACT', 'DEFAULT') AND EXISTS (SELECT * FROM classifier c2 WHERE c2.type = 'INTERFACE'
                                                                      AND EXISTS (SELECT * FROM method m WHERE m.classifier_id = c1.id 
                                                                      AND EXISTS (SELECT * FROM methodinvocation mi WHERE mi.classifier_id = c2.id AND mi.method_id = m.id)));
 
 
-
+-- adapter
+SELECT * FROM classifier c WHERE c.type = 'INTERFACE' AND EXISTS (SELECT * FROM derivation d1 WHERE d1.target_id = c.id 
+                                                      AND EXISTS (SELECT * FROM method m WHERE m.classifier_id = d1.source_id
+                                                      AND EXISTS (SELECT * FROM classifier c2 WHERE c2.type IN ('DEFAULT', 'ABSTRACT')
+                                                      AND EXISTS (SELECT * FROM methodinvocation mi WHERE mi.classifier_id = c2.id AND mi.method_id = m.id))));
--- src/parser/Main.java
+++ src/parser/Main.java
@@ -3,7 +3,7 @@ package parser;
 public class Main {
 
 	public static void main(String[] args) {
-		UMLParser parser = new UMLParser("/home/ludwig/Repositories/Study/Seminar/code/workspace-papyrus/Program/test.uml");
+		UMLParser parser = new UMLParser("/home/ludwig/Repositories/Study/Seminar/code/workspace-papyrus/Program/Program.uml");
 		parser.parse();
 	}
 
